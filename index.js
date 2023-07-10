6acf5d4556ef01d983e3351844a173c8c8562a5d
ansachuk <ansachuk@icloud.com>
2023-07-10 21:01:44 +0300

    full finished hw

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
 import { listContacts, getContactById, addContact, removeContact } from "./contacts.js";
 
-const invokeAction = async ({ action, id, name, email, phone }) => {};
+import { Command } from "commander";
+const program = new Command();
+program
+	.option("-a, --action <type>", "choose action")
+	.option("-i, --id <type>", "user id")
+	.option("-n, --name <type>", "user name")
+	.option("-e, --email <type>", "user email")
+	.option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+	switch (action) {
+		case "list":
+			const list = await listContacts();
+			console.log("list", list);
+			break;
+
+		case "get":
+			const userById = await getContactById(id);
+			console.log("userById", userById);
+			break;
+
+		case "add":
+			const addedUser = await addContact({ id, name, email, phone });
+			console.log("addedUser", addedUser);
+			break;
+
+		case "remove":
+			const deletedUser = await removeContact(id);
+			console.log("deletedUser", deletedUser);
+			break;
+
+		default:
+			console.warn("\x1B[31m Unknown action type!");
+	}
+};
+
+invokeAction(argv);
